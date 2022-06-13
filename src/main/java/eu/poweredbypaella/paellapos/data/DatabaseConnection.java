5fca54d8abb030f29d093c98e1cdcddc213b83b5
Wes Taylor <jamsterwes@gmail.com>
2022-06-13 16:43:58 +0200

    added getItem / connection to DatabaseConnection (pp r/w)

--- src/main/java/eu/poweredbypaella/paellapos/data/DatabaseConnection.java
+++ src/main/java/eu/poweredbypaella/paellapos/data/DatabaseConnection.java
@@ -1,9 +1,40 @@
 package eu.poweredbypaella.paellapos.data;
 
+import java.net.URL;
+import java.sql.*;
+
 public class DatabaseConnection {
 
+    Connection conn;
+
+    private final String URL = "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315950_3db";
+    private final String USERNAME = "csce315950_3user";
+    private final String PASSWORD = "team3three";
+
+    public DatabaseConnection() {
+        try {
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
     public Item getItem(int id) {
-        return null;
+        try {
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT display_name, unit_price, by_weight FROM items WHERE id=" + id + ";";
+            ResultSet result = stmt.executeQuery(sql);
+            return new Item(result.getString("display_name"),
+                            result.getDouble("unit_price"),
+                            result.getBoolean("by_weight"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+            return null;
+        }
     }
 
     public void setQuantity(int id) {
