5654bc3460f7986e1d8491e351a8f7502598fcdc
Carlos Carrillo <90294685+cjst3ch@users.noreply.github.com>
2022-06-13 09:38:27 -0500

    Home Page started

--- src/main/java/eu/poweredbypaella/paellapos/HelloApplication.java
+++ src/main/java/eu/poweredbypaella/paellapos/HelloApplication.java
@@ -3,6 +3,7 @@ package eu.poweredbypaella.paellapos;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +11,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login_page.fxml"));
+        int width = (int) Screen.getPrimary().getBounds().getWidth()/2;
+        int height = (int) Screen.getPrimary().getBounds().getHeight()/2;
+
+        Scene scene = new Scene(fxmlLoader.load(), width, height);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.show();
     }
 
