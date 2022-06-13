58a65644b863d79cc959f21418c8b20e170ec75e
Wes Taylor <jamsterwes@gmail.com>
2022-06-13 16:23:39 +0200

    added employee constructor

--- src/main/java/eu/poweredbypaella/paellapos/data/Employee.java
+++ src/main/java/eu/poweredbypaella/paellapos/data/Employee.java
@@ -4,4 +4,10 @@ public class Employee {
     public String name;
     public int id;
     public boolean isAdmin;
+
+    public Employee(String name, int id, boolean isAdmin) {
+        this.name = name;
+        this.id = id;
+        this.isAdmin = isAdmin;
+    }
 }
