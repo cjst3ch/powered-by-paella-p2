e1680b57c12fa045133b35f7bc34a578532c4291
Wes Taylor <jamsterwes@gmail.com>
2022-06-14 20:44:07 +0200

    populated checkout and inventory management page

--- src/main/java/eu/poweredbypaella/paellapos/controllers/CheckoutPageController.java
+++ src/main/java/eu/poweredbypaella/paellapos/controllers/CheckoutPageController.java
@@ -1,4 +1,59 @@
 package eu.poweredbypaella.paellapos.controllers;
 
+import eu.poweredbypaella.paellapos.data.DatabaseConnection;
+import eu.poweredbypaella.paellapos.data.Item;
+import eu.poweredbypaella.paellapos.data.Receipt;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class CheckoutPageController {
+
+    // Back button
+    @FXML
+    protected Button backButton;
+
+    // Employee name title in top of page
+    @FXML
+    public Label employeeNameLabel;
+
+    // Receipt table
+    @FXML
+    public TableView<Item> itemTable;
+    @FXML
+    public TableColumn<Item, String> nameColumn;
+    @FXML
+    public TableColumn<Item, Double> unitPriceColumn;
+    @FXML
+    public TableColumn<Item, Double> quantityColumn;
+
+    // Item input
+    @FXML
+    public TextField itemPurchased;
+    @FXML
+    public TextField purchaseQuantity;
+
+    // Card/cash buttons
+    @FXML
+    public Button cardButton;
+    @FXML
+    public Button cashButton;
+
+    // Total display
+    @FXML
+    public TextField purchaseTotal;
+
+    private DatabaseConnection db;
+
+    private int employeeID = 2;
+
+    // Local receipt/isCash
+    private Receipt currentReceipt = new Receipt();
+    private boolean isCash = false;
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+
 }
