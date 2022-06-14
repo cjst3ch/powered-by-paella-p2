6e4cf253ebe6feed62a27de0f1835c114e51fa05
azail <azail@tamu.edu>
2022-06-14 18:42:51 +0200

    On action Scene Switching functions to Controller .PP with Carlos

--- src/main/java/eu/poweredbypaella/paellapos/HelloController.java
+++ src/main/java/eu/poweredbypaella/paellapos/HelloController.java
@@ -1,7 +1,15 @@
 package eu.poweredbypaella.paellapos;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -62,4 +70,64 @@ public class HelloController {
     protected void onPlaceOrderClick() {}
 
 
+    //  NAVIGATION SCENE SWITCHING
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToManagerMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("manager_menu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToCheckoutPage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("checkout_page.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToLoginPage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("login_page.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToInventoryManagement(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("inventory_management.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToOrderList(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("order_list.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToOrderPlacement(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("order_placements.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToReceipts(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("receipts.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
